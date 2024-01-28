adf9071c8e72866f3349e29d41c20ffd3af126d3
NutJaroensri <44718224+NutJaroensri@users.noreply.github.com>
2024-01-27 20:28:07 -0800

    Add files via upload

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var projects = new List<Project>() { 
+                new Project { Project_id = 1, Name = "Project 1", Description = "First Project" },
+                new Project { Project_id = 2, Name = "Project 2", Description = "Second Project"}
+            };
+            return View(projects);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var project = new Project { Project_id = id, Name = "Project " + id, Description = "Details of project " + id }
+            return View(project);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Project project)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
