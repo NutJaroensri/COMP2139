7d345791bc6b920c10bf5117db8e0238a7fd76ef
NutJaroensri <44718224+NutJaroensri@users.noreply.github.com>
2024-02-28 01:13:38 +0700

    Add files via upload

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,21 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using WebApplication1.Models;
-
-namespace WebApplication1.Data
+using WebApplication2.Models;
+namespace WebApplication2.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+        
+        }
         public DbSet<Project> Projects { get; set; }
         public DbSet<ProjectTask> ProjectTasks { get; set; }
-
-
-
-
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Project>().HasKey(p => p.Project_id);
-        }
     }
 }
